db9b09de118ef952e00d3d9d576d8365b2781c96
Xiao___ <a43761854@qq.com>
2021-08-29 16:09:08 +0900

    1609

--- Spritel.py
+++ Spritel.py
@@ -7,6 +7,20 @@
 import cocos
 from cocos.director import director
 import pyglet
+from pyglet.window import key
+
+
+#移动class
+class Mover(cocos.actions.Move):
+    def step(self, dt):
+        super().step(dt)
+        print(keyboard)
+        vel_x = (keyboard[key.RIGHT] - keyboard[key.LEFT]) * 500
+        vel_y = (keyboard[key.UP] - keyboard[key.DOWN]) * 500
+        self.target.velocity = (vel_x, vel_y)
+        
+
+
 
 class Spritel(cocos.layer.Layer):
     def __init__(self):
@@ -25,11 +39,14 @@ class Spritel(cocos.layer.Layer):
         #将动图加入图层（laeyr）
         spr = cocos.sprite.Sprite(anim)
         spr.position = 400, 360
-        
+
+        #设定图层速度
+        spr.velocity = (0,0)
+        #执行移动
+        spr.do(Mover())
+
         self.add(spr)   
         
-
-
 
 class Spritel2(cocos.sprite.Sprite):
     def __init__(self):
@@ -38,12 +55,23 @@ class Spritel2(cocos.sprite.Sprite):
         
         self.position = 600, 360
 
+class BackgroundLayer(cocos.layer.ScrollableLayer):
+    def __init__(self, parallax):
+        super().__init__(parallax=parallax)
+
 
 
 if __name__ == '__main__':
 
     director.init(1280,720,'new')
+    
+    #设定键盘变量
+    keyboard = key.KeyStateHandler()
+    #读取键盘
+    director.window.push_handlers(keyboard)
+    
 
+    
     spr1_layer = Spritel()
     spr2_layer = Spritel2()
     
@@ -51,7 +79,7 @@ if __name__ == '__main__':
     test_scene = cocos.scene.Scene()
     #将layer加入scenne
     test_scene.add(spr1_layer, name="1")
-    test_scene.add(spr2_layer, name="player")
+    #test_scene.add(spr2_layer, name="player")
     
 
     director.run(test_scene)
